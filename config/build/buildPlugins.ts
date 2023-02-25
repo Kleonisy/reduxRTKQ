56825c5b9aa5189de58ce90248b7ec159805cb08
Kleonisy <neron0195@mail.ru>
2023-02-25 17:20:01 +0300

    feat: add bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,12 +1,16 @@
 import webpack from 'webpack'
 import HTMLWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 import { IBuildOptions } from './types/config'
 
 export function buildPlugins({ paths, isDev }: IBuildOptions): webpack.WebpackPluginInstance[] {
   return [
     new HTMLWebpackPlugin({
       template: paths.html
+    }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
     }),
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
