85f83799042becf6c684a8d1d3fb8cbf0ca4d180
Kleonisy <neron0195@mail.ru>
2023-03-12 15:39:07 +0300

    feat: add scripts to configure reg-cli html report for ui screenshot testing

---
+++ scripts/generate-visual-json-report.js
@@ -0,0 +1,28 @@
+const { promisify } = require('util')
+const { readdir, writeFile } = require('fs')
+const { join: joinPath, relative } = require('path')
+
+const asyncReaddir = promisify(readdir)
+const writeFileAsync = promisify(writeFile)
+
+const lokiDir = joinPath(__dirname, '..', '.loki')
+const actualDir = joinPath(lokiDir, 'current')
+const expectedDir = joinPath(lokiDir, 'reference')
+const diffDir = joinPath(lokiDir, 'difference');
+
+// eslint-disable-next-line wrap-iife
+(async function main() {
+  const diffs = await asyncReaddir(diffDir)
+  await writeFileAsync(joinPath(lokiDir, 'report.json'), JSON.stringify({
+    actualDir: relative(lokiDir, actualDir),
+    actualItems: diffs,
+    deletedItems: [],
+    diffDir: relative(lokiDir, diffDir),
+    diffItems: diffs,
+    expectedDir: relative(lokiDir, expectedDir),
+    expectedItems: diffs,
+    failedItems: diffs,
+    newItems: [],
+    passedItems: []
+  }))
+})()
